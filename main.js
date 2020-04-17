60880fd684979d43500eeb240a1cf85f373b34a5
WenKang <wenkang11861@gmail.com>
2020-04-17 15:32:11 +0800

    First Commit, need to rework the face rec

---
+++ main.js
@@ -0,0 +1,22 @@
+  
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const ipc = electron.ipcMain;
+const dialog = electron.dialog;
+const {app, BrowserWindow, Menu, globalShortcut, shell, webContents} = require('electron');
+
+let mainWindow;
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({hasShadow: false, frame: true, minWidth: 200 ,minHeight: 45, webPreferences: {nodeIntegration:true}});
+    mainWindow.setPosition(1720,0);
+    mainWindow.setSize(200,45);
+    mainWindow.webContents.openDevTools();
+    Menu.setApplicationMenu(null);
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protcol:'file',
+        slashes: true
+    }));
+});
